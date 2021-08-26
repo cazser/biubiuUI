1ffb4c2633389ee1a44e81c672763ab5fa30e399
cazser <2264712072@qq.com>
2021-08-27 07:45:47 +0800

    试图用parcel打包失败

---
+++ src/app.js
@@ -0,0 +1,11 @@
+ import Vue from 'vue'
+ import Button from './button'
+ Vue.component('biu-button', Button);
+ new Vue({
+         el: '#app',
+         data: {
+             message: 'hi'
+         }
+     }
+
+ )
---
+++ src/button.vue
@@ -0,0 +1,37 @@
+<template>
+	<button class="biu-button">hi</button>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+ 
+    .biu-button {
+        height: var(--button-height);
+        font-size: var(--font-size);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+	
+	&:hover {
+        border-color: var(--button-color-hover);
+   	}
+    
+    	&:active {
+    	    background-color: var(--button-active-bg);
+    	}
+    
+    	&:focus {
+    	    outline: none;
+    	}
+    }
+    
+   
+</style>
