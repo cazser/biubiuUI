0139143aeb55f172d24d8d32a573606b7893eb4c
cazser <2264712072@qq.com>
2021-08-28 09:21:57 +0800

    创建button-group

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
+//import Vue from 'vue';
 Vue.component('biu-button', Button);
 Vue.component('biu-icon', Icon);
+Vue.component('biu-button-group', ButtonGroup)
 new Vue({
         el: '#app',
         data: {
---
+++ src/button-group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="biu-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+.biu-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .biu-button {
+    border-radius: 0;
+    &:first-child {
+      border-top-left-radius: var(--button-radius);
+      border-bottom-left-radius: var(--button-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+    &:last-child {
+      border-top-right-radius: var(--button-radius);
+      border-bottom-right-radius: var(--button-radius);
+    }
+  }
+}
+</style>
