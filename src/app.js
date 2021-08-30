1589d807b498f6f91c8d23978db41844dbc2728c
cazser <2264712072@qq.com>
2021-08-30 17:27:08 +0800

    开始写input组件

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
+import Input from './input.vue'
 import chai from 'chai';
 import spies from 'chai-spies'
+
 //import Vue from 'vue';
 Vue.component('biu-button', Button);
 Vue.component('biu-icon', Icon);
 Vue.component('biu-button-group', ButtonGroup)
+Vue.component('biu-input', Input)
 new Vue({
         el: '#app',
         data: {
@@ -16,55 +19,3 @@ new Vue({
     }
 
 )
-
-chai.use(spies)
-    //单元测试
-    {
-        const constructor = Vue.extend(Button)
-        const vm = new constructor({
-            propsData: {
-                icon: "settings"
-            }
-        });
-        vm.$mount()
-        let useElement = vm.$el.querySelector('use')
-        chai.expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
-    } {
-        const constructor = Vue.extend(Button)
-        const vm = new constructor({
-            propsData: {
-                icon: "settings",
-                loading: true
-            }
-        });
-        vm.$mount()
-        let useElement = vm.$el.querySelector('use')
-        chai.expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
-    } {
-        const div = document.createElement('div')
-        document.body.appendChild(div)
-        const constructor = Vue.extend(Button)
-        const vm = new constructor({
-            propsData: {
-                icon: "settings"
-            }
-        });
-        vm.$mount(div)
-        let svg = vm.$el.querySelector('svg')
-        let { order } = window.getComputedStyle(svg)
-
-        chai.expect(order).to.eq("1")
-    } {
-        const constructor = Vue.extend(Button)
-        const vm = new constructor({
-            propsData: {
-                icon: "settings"
-            }
-        });
-        vm.$mount("#test")
-        let spy = chai.spy(function() {})
-        vm.$on('click', spy())
-        let button = vm.$el
-        button.click();
-        chai.expect(spy).to.have.been.called()
-    }
---
+++ src/input.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <input :value="value" type="text"/>
+  </div>
+</template>
+<script>
+export default {
+name:'biu-input',
+props:{
+value:{
+type: String
+}
+}
+};
+</script>
+<style  lang="scss">
+$font-size: 12px;
+$box-shadow-color: rgba(0,0,0,0.5);
+.wrapper{
+  display: inline-block;
+  >input{
+  border: 1px solid #999;
+  height: 32px;
+  border-radius: 4px;
+  padding: 0 8px;
+  font-size: $font-size;
+  &:focus{
+    box-shadow: inset 0 1px 3px $box-shadow-color;
+    outline:none;
+  }
+  }
+  }
+</style>
