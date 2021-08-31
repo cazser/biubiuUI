2ddebd58d862cfdc674d452d48671f12ad2e8939
cazser <2264712072@qq.com>
2021-08-31 20:42:33 +0800

    使用scss生成col样式

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 import chai from 'chai';
 import spies from 'chai-spies'
 
@@ -10,6 +12,8 @@ Vue.component('biu-button', Button);
 Vue.component('biu-icon', Icon);
 Vue.component('biu-button-group', ButtonGroup)
 Vue.component('biu-input', Input)
+Vue.component('biu-row', Row);
+Vue.component('biu-col', Col);
 new Vue({
         el: '#app',
         data: {
---
+++ src/col.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="col">
+		<slot></slot>
+	</div>
+</template>
+<script>
+export default{
+
+};
+</script>
+<style scoped lang="scss">
+	.col{
+	height: 100px;
+	background: grey;
+	width: 100%;
+	border: 1px solid black;
+
+	$class: col-;
+	@for $n from 1 through 24{
+	&.#{$class}#{$n}{
+	 width: ($n /24)*100%;
+	 }
+	}
+	}
+
+</style>
